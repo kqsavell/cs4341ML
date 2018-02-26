dd03e37820f042ac28b62bc18e7c78c9426c263d
Kyle Savell <kqsavell@wpi.edu>
2018-02-25 20:49:27 -0500

    K-nearest vizualization.

--- template.py
+++ template.py
@@ -8,7 +8,9 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 from sklearn.neighbors import KNeighborsClassifier
+import matplotlib.pyplot as plt
 import numpy as np
+import itertools
 
 # Symbolic Constants
 K_LOW = 5
@@ -132,6 +134,44 @@ class DecisionTreeClassification:
         return predict
 
 
+# Function from scikit-learn documentation:
+# http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html#sphx-glr-auto-examples-model-selection-plot-confusion-matrix-py
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+
+        """
+        This function prints and plots the confusion matrix.
+        Normalization can be applied by setting `normalize=True`.
+        """
+        if normalize:
+            cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+            print("Normalized confusion matrix")
+        else:
+            print('Confusion matrix, without normalization')
+
+        print(cm)
+
+        plt.imshow(cm, interpolation='nearest', cmap=cmap)
+        plt.title(title)
+        plt.colorbar()
+        tick_marks = np.arange(len(classes))
+        plt.xticks(tick_marks, classes, rotation=45)
+        plt.yticks(tick_marks, classes)
+
+        fmt = '.2f' if normalize else 'd'
+        thresh = cm.max() / 2.
+        for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+            plt.text(j, i, format(cm[i, j], fmt),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+
+        plt.tight_layout()
+        plt.ylabel('True label')
+        plt.xlabel('Predicted label')
+
+
 # K-Nearest Neighbors
 def k_nearest():
     cur_k = K_LOW
@@ -204,7 +244,19 @@ def main():
     print(confusion_matrix(validation_set_l, dt2.predict(validation_set_f)))
 
     # K-Nearest Neighbors
-    # k_nearest()
+    cf = k_nearest()
+    class_labels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    # Plot non-normalized confusion matrix
+    plt.figure()
+    plot_confusion_matrix(cf, classes=class_labels,
+                          title='Confusion matrix, without normalization')
+
+    # Plot normalized confusion matrix
+    plt.figure()
+    plot_confusion_matrix(cf, classes=class_labels, normalize=True,
+                          title='Normalized confusion matrix')
+
+    plt.show()
 
 
 main()
